a44d33d98c4d8a953d13550e0d5672bc837f5828
yusuke.amada <uutfghut@gmail.com>
2020-11-01 09:00:38 +0000

    CRUD tasks

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -15,12 +15,11 @@
         </tr>
     </table>
     {{-- タスク編集ページへのリンク --}}
-    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
+    {!! link_to_route('tasks.edit', 'このタスク内容を編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
 
     {{-- メッセージ削除フォーム --}}
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
-@endsection
 
 @endsection
